ef5cbc4423868f1f698ed3b04aeec185a69a72fd
Mateus Toledo <mateustoledo30@gmail.com>
2020-06-05 12:18:16 -0300

    migration att

--- moviesTrimble/src/main/resources/db/migration/V1__init.sql
+++ moviesTrimble/src/main/resources/db/migration/V1__init.sql
@@ -12,7 +12,32 @@ VALUES (
     'The Fellowship of the Ring',
     '2001-12-10',
     'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron',
-    9
+    10
+);
+
+INSERT INTO movies
+    (title, release_date, synopsis, usr_rating)
+VALUES (
+        'Pulp Fiction',
+        '1994-05-06',
+            'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption',
+        9
+);
+
+INSERT INTO movies
+    (title, release_date, synopsis, usr_rating)
+VALUES ('The Godfather',
+        '1972-10-05',
+        'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son',
+        9
+);
+
+INSERT INTO movies
+    (title, release_date, synopsis, usr_rating)
+VALUES ('The Dark Knight',
+        '2008-11-05',
+        'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice',
+        10
 );
 
 
